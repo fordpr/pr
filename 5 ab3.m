6b8ace1f8a4eb4559d2f36ad0f1c8f47e3f974db
fordpr <159081513+fordpr@users.noreply.github.com>
2024-02-06 11:00:02 +0100

    Create 5 ab3.m

---
+++ 5 ab3.m
@@ -0,0 +1,19 @@
+function [osax,vysledek] = ab3(a,b,n,poc)  %eto pryam ono doljno byt`
+
+   h = (b-a)/n;
+   fi = zeros(1,n+1);
+   ox = zeros(1,n+1);
+   ox(1) = a;
+   fi(1)=poc;
+   i=2;
+   ox(i)=a+(i-1)*h;
+   %fi(i)=fi(i-1)+h*ABdrce(a+(i-2)*h,fi(i-1));
+   fi(i)=fi(i-1)+h*drce(a+(i-2)*h+h/2,fi(i-1)+h/2*drce(a+(i-2)*h,fi(i-1)));
+   for i=3:n+1
+       ox(i)= a+(i-1)*h; 
+       fi(i) = fi(i-1)+3*h/2*drce(ox(i-1),fi(i-1))- h/2 * drce(ox(i-2),fi(i-2));
+   end
+   osax = ox;
+   vysledek = fi;
+  
+end
